77b8070ff4c16796bfc8f8432c8f0111f01a2d6a
Michael Levit <michael@videogpu.com>
2017-08-19 21:11:14 +0300

    Added delay, removed errout

--- hdl/one_lane_D_PHY.vhd
+++ hdl/one_lane_D_PHY.vhd
@@ -20,8 +20,8 @@ entity one_lane_D_PHY is generic (
      hs_mode_flag : out STD_LOGIC; --goes high when entering HS mode
      output_valid : out STD_LOGIC;-- indicates that the output is valid
      hs_out : out STD_LOGIC_VECTOR (DATA_WIDTH_OUT - 1 downto 0);
-     lp_out : out STD_LOGIC_VECTOR(1 downto 0); --bit 1 = Dp line, bit 0 = Dn line
-     err_occured : out STD_LOGIC  --active highl 0 = no error, 1 - error acured
+     lp_out : out STD_LOGIC_VECTOR(1 downto 0) --bit 1 = Dp line, bit 0 = Dn line
+     --err_occured : out STD_LOGIC  --active highl 0 = no error, 1 - error acured
      );
 end one_lane_D_PHY;
 
@@ -32,14 +32,21 @@ constant LP_01 : std_logic_vector(1 downto 0) := "01"; --Control mode: HS-Rqst
 constant LP_10 : std_logic_vector(1 downto 0) := "10"; --Control mode: LP-Rqst
 constant LP_11 : std_logic_vector(1 downto 0) := "11"; --Control mode: Stop
 constant COUNTER_WIDTH : integer := 8; --max 256*10ns(100Mhz clock) = 2560ns delay
+constant tLPX : integer := 5; --5 clock cycles of 100 Mhz = 50 ns
+constant tHSprepare : integer := 6; --6 clock cycles of 100 Mhz = 60 ns
+constant tHSzero : integer := 8; --8 clock cycles of 100 Mhz = 80 ns
 
-type state_type is (CTRL_Stop,CTRL_Bridge,CTRL_HS_Rqst,CTRL_LP_Rqst,HS_Go,HS_Sync,HS_Burst);
+
+
+
+type state_type is (CTRL_Stop,CTRL_Bridge,CTRL_HS_Rqst,CTRL_LP_Rqst,HS_Go,HS_Burst);
 signal state_reg, state_next : state_type := CTRL_Stop;
 signal lp_reg,lp_next :  STD_LOGIC_VECTOR(1 downto 0) := LP_11;
 signal hs_mode_flag_reg,hs_mode_flag_next : STD_LOGIC := '0'; --default LS mode
 signal hs_out_reg,hs_out_next : STD_LOGIC_VECTOR (DATA_WIDTH_OUT - 1 downto 0) := (others => '0');
 signal ready_to_transmit_reg,ready_to_transmit_next : STD_LOGIC := '0'; 
-signal reset_conter : STD_LOGIC := '0';
+signal reset_conter_reg,reset_conter_next : STD_LOGIC := '0';
+signal is_sync_sent_reg,is_sync_sent_next : STD_LOGIC := '0';
 signal counter_value :  STD_LOGIC_VECTOR (COUNTER_WIDTH - 1 downto 0) := (others => '0');
 --components
 component counter generic(n: natural :=COUNTER_WIDTH);
@@ -55,7 +62,7 @@ begin
 delay_counter: counter 
     generic map(n => COUNTER_WIDTH)
     port map(clk => clk_100Mhz,
-             rst => reset_conter,
+             rst => reset_conter_reg,
              counter_out => counter_value);           
 
 --end of components instantinations
@@ -74,12 +81,16 @@ begin
 			hs_mode_flag_reg <= '0';
 			hs_out_reg <=  (others => '0');
 			ready_to_transmit_reg <= '0';
+			reset_conter_reg <= '1';
+			is_sync_sent_reg <= '0';
 		elsif (clk'event and clk = '1') then 		
 			state_reg <= state_next;
 			lp_reg <= lp_next;
 			hs_mode_flag_reg <= hs_mode_flag_next;
 			hs_out_reg <= hs_out_next;
 			ready_to_transmit_reg <= ready_to_transmit_next;
+			reset_conter_reg <= reset_conter_next;
+			is_sync_sent_reg <= is_sync_sent_next;
 		end if;
 						
 end process; --FSMD_state
@@ -87,7 +98,8 @@ end process; --FSMD_state
 
 --video output state machine, section 5.4, page 35
 D_PHY_FSMD : process(state_reg,lp_reg,hs_mode_flag_reg,hs_out_reg,data_in,
-                     start_transmission,stop_transmission,ready_to_transmit_reg)
+                     start_transmission,stop_transmission,ready_to_transmit_reg,
+                     counter_value,is_sync_sent_reg)
 begin
 
     state_next <= state_reg;
@@ -95,38 +107,56 @@ begin
     hs_mode_flag_next <= hs_mode_flag_reg;
     hs_out_next <= hs_out_reg;
     ready_to_transmit_next <= ready_to_transmit_reg;
+    reset_conter_next <= '0'; --no counter reset by default
+    is_sync_sent_next <= is_sync_sent_reg;
     
     case state_reg is 
             when CTRL_Stop =>
                  ready_to_transmit_next <= '0';
-                 hs_mode_flag_next <= '0';                 
+                 hs_mode_flag_next <= '0';            
+                 is_sync_sent_next <= '0';           
                  if (start_transmission = '1') then
                  state_next <= CTRL_HS_Rqst;
-                 lp_next <= LP_01;                          
+                 lp_next <= LP_01;                    
+                 reset_conter_next  <= '1';      
                  end if;
             when CTRL_HS_Rqst =>
-                 --wait T_LPX = minimum 50 ns.
-                 state_next <= CTRL_Bridge;   
-                 lp_next <= LP_00;              
-                 hs_mode_flag_next <= '0';
+                 --wait tLPX = minimum 50 ns.
+                 if (to_integer(unsigned(counter_value)) = tLPX) then
+                    state_next <= CTRL_Bridge;   
+                    lp_next <= LP_00;              
+                    hs_mode_flag_next <= '0';
+                    reset_conter_next  <= '1';
+                 end if;
             when CTRL_Bridge =>  
-                  --wait THS-PREPARE = min 40 ns + 4*UI,max 85 ns + 6*UI, UI (1 HS bit time) < 10 ns
-                 hs_mode_flag_next <= '1';
-                 hs_out_next <= (others => '0');
-                 state_next <= HS_Go;
+                  --wait tHSprepare = min 40 ns + 4*UI,max 85 ns + 6*UI, UI (1 HS bit time) < 10 ns
+                 if (to_integer(unsigned(counter_value)) = tHSprepare) then
+                    hs_mode_flag_next <= '1';
+                    hs_out_next <= (others => '0');
+                    state_next <= HS_Go;
+                    reset_conter_next  <= '1';
+                 end if;
             when HS_Go =>     
-                  --wait THS-ZERO ~80 ns => (THS-PREPARE + THS-ZERO = min 145 ns + 10*UI)
-                  state_next <= HS_Sync;
-            when HS_Sync =>
-                  hs_out_next <= "00011101";  --output sync sequence '00011101'
-                  ready_to_transmit_next <= '1';
-                  state_next <= HS_Burst;
-            when HS_Burst =>                
-                 hs_out_next <= data_in;  --output payload data 
-                 if ( stop_transmission <= '1') then
+                  --wait tHSzero ~80 ns => (THS-PREPARE + THS-ZERO = min 145 ns + 10*UI)
+                  hs_out_next <= (others => '0');
+                  if (to_integer(unsigned(counter_value)) = tHSzero) then
+                     state_next <= HS_Burst;
+                     reset_conter_next  <= '1';
+                     ready_to_transmit_next <= '1'; --let to sender some time to prepare (2 clock cycles)
+                  end if;
+            when HS_Burst =>   
+                  if (is_sync_sent_reg = '0') then
+                     hs_out_next <= "00011101";  --output sync sequence '00011101'
+                     is_sync_sent_next <= '1';                     
+                  else 
+                     hs_out_next <= data_in;  --output payload data 
+                  end if;
+                 
+                 if ( stop_transmission = '1') then
                     state_next <= CTRL_Stop;
                     ready_to_transmit_next <= '0';
                     hs_mode_flag_next <= '0';
+                    is_sync_sent_next <= '0';
                   end if;
             when CTRL_LP_Rqst =>                 
                  state_next <= CTRL_Stop;       
