2fdfd2bf1d462bd37e735d1225959e21cf2c53a2
Michael Levit <videogpu@github.com>
2017-09-23 00:30:37 -0700

    before cheking CRC with downto

--- simulation/test_send_video_line.vhd
+++ simulation/test_send_video_line.vhd
@@ -35,8 +35,22 @@ COMPONENT send_video_line is
 	 );
 END COMPONENT;
 
+COMPONENT csi_rx_packet_handler is
+    Port ( clock : in STD_LOGIC; --word clock in
+           reset : in STD_LOGIC; --asynchronous active high reset
+           enable : in STD_LOGIC; --active high enable
+           data : in STD_LOGIC_VECTOR (31 downto 0); --data in from word aligner
+           data_valid : in STD_LOGIC; --data valid in from word aligner
+           sync_wait : out STD_LOGIC; --drives byte and word aligner wait_for_sync
+           packet_done : out STD_LOGIC; --drives word aligner packet_done
+           payload_out : out STD_LOGIC_VECTOR(31 downto 0); --payload out from long video packets
+           payload_valid : out STD_LOGIC; --whether or not payload output is valid (i.e. currently receiving a long packet)
+           vsync_out : out STD_LOGIC; --vsync output to timing controller
+           in_frame : out STD_LOGIC; --whether or not currently in video frame (i.e. got FS but not FE)
+		   in_line : out STD_LOGIC); --whether or not receiving video line
+END COMPONENT;
 
-
+--send_video_line
 --Inputs
 signal clk : std_logic; --word clock in
 signal rst : std_logic; --asynchronous active high reset
@@ -51,7 +65,26 @@ signal csi_data_out :   std_logic_vector(7 downto 0); --one byte of CSI stream t
 signal transmission_finished :  std_logic; --rised once the header, payload and footer data is sent.
 signal data_out_valid :  std_logic --goes high when csi_data_out is valid	 
 
+--csi_rx_packet_handler
+--Inputs
+signal enable :  std_logic := '0';  --active high enable
+signal data :  std_logic_vector (31 downto 0); --data in from word aligner
+signal data_valid : std_logic := '0'; --data valid in from word aligner
+--Outputs
+signal sync_wait :  std_logic; --drives byte and word aligner wait_for_sync
+signal packet_done :  std_logic; --drives word aligner packet_done
+signal payload_out :  std_logic_vector(31 downto 0); --payload out from long video packets
+signal payload_valid :  std_logic; --whether or not payload output is valid (i.e. currently receiving a long packet)
+signal vsync_out :  std_logic; --vsync output to timing controller
+signal in_frame :  std_logic; --whether or not currently in video frame (i.e. got FS but not FE)
+signal in_line :  std_logic; --whether or not receiving video line
+
 constant clk_period : time := 10 ns; --LP = 100 Mhz
+
+type arr_of_crc is array (0 to 23) of std_logic_vector(0 to 7);
+signal crc_arr1 : arr_of_crc := (x"FF",x"00",x"00",x"02",x"B9",x"DC",x"F3",x"72",
+                                 x"BB",x"D4",x"B8",x"5A",x"C8",x"75",x"C2",x"7C",
+                                 x"81",x"F8",x"05",x"DF",x"FF",x"00",x"00",x"01");
 
 begin
 
@@ -66,6 +99,20 @@ uut: send_video_line PORT MAP(clk => clk,
 	 transmission_finished => transmission_finished,
 	 data_out_valid => data_out_valid);
 
+
+inst_packet_handler: csi_rx_packet_handler PORT MAP(
+           clock => clk,
+           reset => rst,
+           enable =>  enable,
+           data => data,
+           data_valid => data_valid,
+           sync_wait => sync_wait,
+           packet_done => packet_done,
+           payload_out => payload_out,
+           payload_valid => payload_valid,
+           vsync_out => vsync_out,
+           in_frame => in_frame,
+		   in_line =>  in_line);
 
 -- Clock process definitions
 clk_process :process
